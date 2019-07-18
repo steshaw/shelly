9839b4bf4664f2b32b081944758aefc23b364203
Steven Shaw <steven@steshaw.org>
2019-07-18 18:55:32 +1000

    🔧 Update steshaw.nix overlay

--- dotfiles/.config/nixpkgs/overlays/steshaw.nix
+++ dotfiles/.config/nixpkgs/overlays/steshaw.nix
@@ -1,6 +1,15 @@
 #
 # HT https://gist.github.com/LnL7/570349866bb69467d0caf5cb175faa74
 #
+# Initially install with
+#  $ nix-env -f '<nixpkgs>' -r -iA userPackages
+#
+# Afterwards, edit the file and run:
+#
+#  $ nix-rebuild
+#
+# See https://gist.github.com/LnL7/570349866bb69467d0caf5cb175faa74#gistcomment-2598159
+#
 # See also:
 #  https://gist.github.com/Widdershin/de023630617c405b033245ced16221f2
 #
@@ -9,25 +18,69 @@
 #
 self: super:
 with builtins; rec {
-/*
-  # Trying to get equivalence to compile...
-  haskellPackages = super.haskellPackages.extend (self: super_: {
-    equivalence = super_.equivalence.overrideDerivation (attrs: {
-      doCheck = false;
-      src = super.fetchgit {
-        url = "https://github.com/pa-ba/equivalence";
-        sha256 = "1dr78i7dsydbi7gxr4864an907xydvmybyya5f5lbgdbv2cy1g7i";
-        rev = "b194d4c4b3edd86d786139fdce99a4d19b9e4b57";
-      };
-    });
-  });
-*/
-  userPackages = super.userPackages or {} // {
 
-    # Packages
-#    Agda = super.haskellPackages.Agda; # Agda is currently broken in nixpkgs
-#    because of equivalence.
-    # XXX: Should I call this `my_vim` instead?
+  userPackages = super.userPackages or {} // super.recurseIntoAttrs {
+#  userPackages = super.userPackages or {} // {
+
+#    Agda = super.haskellPackages.Agda; # Agda is broken.
+
+    #
+    # Kind of like build-essential.
+    #
+    # For example needed to build some NPM packages.
+    #
+    # FIXME: Cannot `yarn global add gitmoji` because kexec doesn't build.
+    #
+    inherit (self)
+      gcc
+      gnumake
+      python
+    ;
+
+    #
+    # Hmmm
+    #
+    yarn = (super.yarn.override {
+      nodejs = super.nodejs-10_x;
+    });
+    inherit (self)
+      nodejs-10_x
+    ;
+
+    #
+    # My favourite command line apps.
+    #
+    inherit (self)
+      bashInteractive_5
+      bat
+      bind # for dig
+      cabal2nix
+      direnv
+      emacs
+      fd
+      fzf
+      gist
+      git
+      gnupg22
+      hledger
+      htop
+      httpie
+      jq
+      mr
+      neofetch
+#      neovim # Deferring to Vim for now.
+      ripgrep
+      rlwrap
+      shellcheck
+      stack
+      tmux
+      tree
+      watchman
+      youtube-dl
+      zsh
+    ;
+
+    # XXX: Do I need to call this `my_vim` instead?
     vim = (super.vim_configurable.override {
       guiSupport = "no";
       darwinSupport = super.stdenv.isDarwin;
@@ -35,40 +88,42 @@ with builtins; rec {
     }).overrideAttrs (prevAttrs: {
       name = "my-vim-${prevAttrs.version}";
     });
+
+    tmux-fzf-url = self.tmuxPlugins.fzf-tmux-url;
+
+    #
+    # GUI apps and fonts.
+    #
     inherit (self)
-      bashInteractive_5
-      bat
-      cabal2nix
-      direnv
-      fzf
-      gist
-      git # Better to use Homebrew git? Better macOS Keychain (i.e. UseKeychain).
-#      gnupg22
-      htop
-      httpie
-#      jq
-      mr
-      neofetch
-#      neovim
-#      ripgrep
-      rlwrap
-#      shellcheck
-#      stack
-      tmux
-      tree
-      watchman
-      youtube-dl
-      zsh
-      ;
+      fira-code
+      source-code-pro
+    ;
 
-   tmux-fzf-url = self.tmuxPlugins.fzf-tmux-url;
-
+    #
     # Default packages.
+    #
     # WARNING: Do not remove!
+    #
     inherit (self)
       nix # Don't enable this on multi-user.
       cacert;
 
+    #
+    # nix-rebuild script.
+    #
+    nix-rebuild = super.writeScriptBin "nix-rebuild" ''
+      #!${super.stdenv.shell}
+      if ! command -v nix-env &>/dev/null; then
+          echo "warning: nix-env was not found in PATH, add nix to userPackages" >&2
+          PATH=${self.nix}/bin:$PATH
+      fi
+      exec nix-env -f '<nixpkgs>' -r -iA userPackages "$@"
+    '';
+
+    #
+    # FIXME: Fix below which provides a diff but breaks with use of `super.recurseIntoAttrs` above.
+    #        The `super.recurseIntoAttrs` allows you to find userPackages when doing `nix search`.
+/*
     nix-rebuild = super.writeScriptBin "nix-rebuild" ''
       #!${super.stdenv.shell}
 
@@ -100,5 +155,6 @@ with builtins; rec {
         versionText = super.lib.strings.concatMapStrings (s: s+"\n") versions;
       in
       super.writeTextDir "package_versions" versionText;
+*/
   };
 }
