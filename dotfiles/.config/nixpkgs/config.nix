55ce1a594a1a38925616234ddd0d45d0042ac314
Steven Shaw <steven@steshaw.org>
2019-07-21 17:54:53 +1000

    🔧 Add NUR

--- dotfiles/.config/nixpkgs/config.nix
+++ dotfiles/.config/nixpkgs/config.nix
@@ -1,3 +1,19 @@
 {
   allowUnfree = true;
+
+  #
+  # Provide packages from Nix User Repository.
+  #
+  # See https://github.com/nix-community/NUR.
+  #
+  packageOverrides = pkgs:
+    let nurPath = builtins.fetchTarball {
+      url = "https://github.com/nix-community/NUR/archive/a77e75c.tar.gz";
+      sha256 = "sha256:11hkn5zcyxzaqlbmd8ck506j8faj0574hc25zq9nx4a27yp37cw6";
+    };
+    in {
+      nur = import nurPath {
+        inherit pkgs;
+      };
+  };
 }
