5d7edfe9183850f87010bd982ef89fc600e23331
Steven Shaw <steven@steshaw.org>
2019-10-21 18:30:54 +1000

    🔧 Nix: tweak NUR package config

--- dotfiles/.config/nixpkgs/config.nix
+++ dotfiles/.config/nixpkgs/config.nix
@@ -7,13 +7,19 @@
   # See https://github.com/nix-community/NUR.
   #
   packageOverrides = pkgs:
-    let nurPath = builtins.fetchTarball {
-      url = "https://github.com/nix-community/NUR/archive/a77e75c.tar.gz";
-      sha256 = "sha256:11hkn5zcyxzaqlbmd8ck506j8faj0574hc25zq9nx4a27yp37cw6";
-    };
+    let
+      old = true;
+      nurPkgs = if old then builtins.fetchTarball {
+        url = "https://github.com/nix-community/NUR/archive/a77e75c.tar.gz";
+        sha256 = "sha256:11hkn5zcyxzaqlbmd8ck506j8faj0574hc25zq9nx4a27yp37cw6";
+      } else builtins.fetchGit {
+        url = "https://github.com/nix-community/NUR";
+        rev = "6b7e611ad65693f954112f7d346bc6b4e779ea08";
+      };
     in {
-      nur = import nurPath {
+      nur = import nurPkgs {
         inherit pkgs;
       };
   };
+
 }
