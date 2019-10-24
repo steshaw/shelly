69b911512df521796016f1d4fd601b08df65a4bb
Steven Shaw <steven@steshaw.org>
2019-10-25 07:28:31 +1000

    🔧 Add debbie

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -167,8 +167,17 @@
     ];
     openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDQM+CdduNpScMe8Uucb5TCVLx3HrXUrTJO8hBkOF8Dy4+IYLFxo6teT8XT4X0+SLJ6gdxPjRPfqmcZwdg051BkaPAh6TkX0zqAeaBFSh3rVmNWV1mxDxYl9X5yWXkaj/kCOpJccz2NrNINYvv4wYHVoVRDg97+RMCdLyzXV2W0sf+J1Mozpj05AAgo6iqUNwo8bHJtekD4UZ6L1Zql3QSwBZvIo2eK9Ir6DPhDMRD0YHLUnFfdLYbGhlqi3qPu8CbEbu+4ptyhlePqvIymdmVwd2VL44SBr5KvlmFuZmhm/LL2b89tb2a2X3RW8do4y1W/wIOwfSeUfXf83zUftTyR steven@steshaw.org" ];
   };
+  users.users.debbie = {
+    isNormalUser = true;
+    home = "/home/debbie";
+    description = "Deborah Shaw";
+    extraGroups = [
+#      "networkmanager"
+    ];
+  };
 
   security.sudo.wheelNeedsPassword = false;
+  nix.trustedUsers = ["@wheel"];
 
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
