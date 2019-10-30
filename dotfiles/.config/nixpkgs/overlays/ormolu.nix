4728ab3f8cbaafe380e5e932fc4371d339951e36
Steven Shaw <steven@steshaw.org>
2019-10-30 18:58:13 +1000

    🔧 Nix: disable ormolu overlay
    Seems to be in nixpkgs now.

--- dotfiles/.config/nixpkgs/overlays/ormolu.nix
+++ dotfiles/.config/nixpkgs/overlays/ormolu.nix
@@ -1,13 +1,17 @@
 # This overlay adds Ormolu straight from GitHub.
 self: super:
-
 let
   source = builtins.fetchGit {
     url = "https://github.com/tweag/ormolu";
     rev = "43c6f4563428e871d760312ed1e414a51224c074"; # update as necessary
   };
   ormolu = import source { pkgs = self; };
-in {
+  # Seems that ormolu is now in nixpkgs, so disable.
+  disable = true;
+in
+if disable
+then {}
+else {
   haskell = super.haskell // {
     packages = super.haskell.packages // {
       "${ormolu.ormoluCompiler}" =
