8dafe6dd9226d7067876d3242b6e36633c3bce3a
Steven Shaw <steven@steshaw.org>
2019-11-18 06:36:03 +1000

    🔧 Nix: disable ormolu

--- dotfiles/.config/nixpkgs/overlays/haskell.nix
+++ dotfiles/.config/nixpkgs/overlays/haskell.nix
@@ -7,5 +7,9 @@ self: super: {
   hlint = self.haskell.lib.enableSeparateBinOutput self.haskellPackages.hlint;
   pointfree =
     self.haskell.lib.enableSeparateBinOutput self.haskellPackages.pointfree;
-  ormolu = self.haskell.lib.enableSeparateBinOutput self.haskellPackages.ormolu;
+
+
+  # XXX: Seems to prevent `nix-env -qa` when used. With the following error:
+  #      error: attribute 'ormolu' missing, at /home/steshaw/.config/nixpkgs/overlays/haskell.nix:10:53
+  ormolu = if false then self.haskell.lib.enableSeparateBinOutput self.haskellPackages.ormolu else {};
 }
