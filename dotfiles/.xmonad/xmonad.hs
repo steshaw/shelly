4bfa0b23c31156a047bb959adc52679aacf55a48
Steven Shaw <steven@steshaw.org>
2020-02-10 07:36:06 +1000

    🔧 Xmonad: configure for MATE

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -2,6 +2,7 @@ module Main where
 
 import XMonad
 import XMonad.Config.Kde
+import XMonad.Config.Mate
 import qualified XMonad.StackSet as W -- to shift and float windows
 
 import Data.Semigroup (Endo)
@@ -12,23 +13,20 @@ myTerminal = let useAlacritty = True in
     then "alacritty"
     else "konsole"
 
+myBasicConfig = def {modMask = mod4Mask, terminal = myTerminal}
+
 -- |
 --
 -- Helpful KDE 5 references:
 --
 -- * <https://wiki.haskell.org/Xmonad/Using_xmonad_in_KDE>
 -- * <https://github.com/marbu/xmonad/issues/1>
-main :: IO ()
-main =
-  let enableKde = False
-   in if enableKde
-        then
-          xmonad
-            kdeConfig
+myKdeConfig = kdeConfig
               { modMask = mod4Mask, -- use the Windows button as mod
                 manageHook = manageHook kdeConfig <+> myManageHook
               }
-        else xmonad def {modMask = mod4Mask, terminal = myTerminal}
+
+myMateConfig = mateConfig {modMask = mod4Mask, terminal = myTerminal, borderWidth = 50}
 
 myManageHook :: Query (Endo WindowSet)
 myManageHook =
@@ -43,3 +41,15 @@ myManageHook =
     myOtherFloats = ["alsamixer"]
     webApps = ["Firefox-bin", "Opera"] -- open on desktop 2
     ircApps = ["Ksirc"] -- open on desktop 3
+
+data MyConfig = Basic | KDE | MATE
+
+-- TODO: Type error!
+{-
+myConfig Basic = myBasicConfig
+myConfig KDE = myKdeConfig
+myConfig MATE = myMateConfig
+-}
+
+main :: IO ()
+main = xmonad myMateConfig
