b41845a9d0d9c474699b518bfaf52691804ae02b
Steven Shaw <steven@steshaw.org>
2020-09-02 17:54:14 +1000

    i3: Use Adwaita-Dark theme

--- etc/nixos/xorg.nix
+++ etc/nixos/xorg.nix
@@ -3,6 +3,32 @@
   # FIXME: Not Xorg.
   programs.sway.enable = false;
 
+/*
+  environment = {
+    etc."xdg/gtk-2.0/gtkrc" = {
+      text = ''
+        gtk-icon-theme-name = "Adwaita"
+        gtk-theme-name = "Arc-Dark"
+        gtk-cursor-theme-name = "Adwaita"
+        gtk-fallback-icon-theme = "gnome"
+        gtk-font-name = "DejaVu Sans 11"
+      '';
+      mode = "444";
+    };
+    etc."xdg/gtk-3.0/settings.ini" = {
+      text = ''
+        [Settings]
+        gtk-icon-theme-name=Arc
+        gtk-theme-name=Arc-Dark
+        gtk-cursor-theme-name=Adwaita
+        gtk-fallback-icon-theme=gnome
+        gtk-font-name = Noto Sans 11
+      '';
+      mode = "444";
+    };
+  };
+*/
+
   # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
@@ -17,15 +43,24 @@
 
     displayManager.sddm.enable = false;
 
+    # configure lightdm
     displayManager = {
       lightdm = {
         enable = true;
-        greeters.enso.enable = false;
+        greeters = {
+          gtk = {
+            #indicators = [ "~clock" "~session" "~power" ];
+            theme = {
+              name = "Adwaita-Dark";
+            };
+          };
+        };
       };
     };
 
     displayManager = {
       defaultSession = "none+i3";
+      #default = "gnome-flashback-gnome3-i3-xsession";
     };
 
     # Enable the KDE Desktop Environment?
@@ -53,6 +88,16 @@
     };
   };
 
+  # Enable Qt5 integration.
+  qt5 = {
+    enable = true;
+    platformTheme = "gtk2";
+    style = "gtk2";
+  };
+  environment.extraInit = ''
+    export QT_STYLE_OVERRIDE="Adwaita-Dark"
+  '';
+
   services.gnome3.gnome-keyring.enable = true;
 
   environment.mate.excludePackages = with pkgs.mate; [
@@ -64,6 +109,8 @@
   security.pam.services.sddm.enableKwallet = true;
   security.pam.services.kdewallet.enableKwallet = true;
   environment.systemPackages = with pkgs; [
+
+  qgnomeplatform adwaita-qt
     # Apps.
     brave
     dropbox
