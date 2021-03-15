dbf601f31ffb53f5d8d417d51ce7da5093697ff2
Steven Shaw <steven@steshaw.org>
2021-03-15 12:02:00 +1000

    Doom: update org-directory

--- dotfiles/.config/doom/config.el
+++ dotfiles/.config/doom/config.el
@@ -29,7 +29,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Code/steshaw/notes/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
