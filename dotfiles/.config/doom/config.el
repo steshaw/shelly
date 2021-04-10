cffbdecaee342213703d3fb7905605c26324cb8c
Steven Shaw <steven@steshaw.org>
2021-04-10 16:07:18 +1000

    Doom Emacs: Trim config

--- dotfiles/.config/doom/config.el
+++ dotfiles/.config/doom/config.el
@@ -23,11 +23,6 @@
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 15 :weight 'normal))
-;   dotspacemacs-default-font '("FiraCode Nerd Font"
-;                               :size 15
-;                               :weight normal
-;                               :width normal
-;                               :powerline-scale 1.1)
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
