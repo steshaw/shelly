8c6c251f4e87f6d388cbef014d1f4313a30c62bd
Steven Shaw <steven@steshaw.org>
2021-07-26 16:30:30 +1000

    Scripts: gbmm

--- dotfiles/.config/fish/functions/gbmm.fish
+++
@@ -1,7 +0,0 @@
-# Renaming the default Git branch from master to main.
-function gbmm
-  git branch -m master main
-  and git fetch origin
-  and git branch --set-upstream-to=origin/main main
-  and git remote set-head origin --auto
-end
