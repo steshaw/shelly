b8ae0de4586f9f8b958e2dcdf32e12748d3d3982
Steven Shaw <steven@steshaw.org>
2021-07-28 15:13:19 +1000

    Fish: Bind / to history search for Bash-like functionality

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,4 +1,9 @@
 if status is-interactive
+  alias ls='exa --classify'
+  alias l='ls --long'
+  alias ll='l --all'
+  alias la='ll --all'
+
   abbr --add --global gaa 'git add --all'
   abbr --add --global gb 'git branch --verbose --verbose'
   abbr --add --global gba 'git branch --all --verbose --verbose'
@@ -11,10 +16,22 @@ if status is-interactive
   abbr --add --global gpfl 'git push --force-with-lease'
   abbr --add --global gcane 'git commit --amend --no-edit'
 
-  alias ls='exa --classify'
-  alias l='ls --long'
-  alias ll='l --all'
-  alias la='ll --all'
+  abbr --add --global grep 'grep --directories=skip'
 
   set CDPATH ~/Code/steshaw ~/Code/ardana ~/Code/iohk ~/Code/mlabs ~/Code
+
+  # vi-style search as in Bash
+  # Cribbed from https://github.com/fish-shell/fish-shell/issues/2271#issuecomment-352210807
+  function reverse_history_search
+    history | fzf --no-sort | read -l command
+    if test $command
+      # Clear the command line buffer first
+      # as a workaround for the buffer appearing
+      # to the empty (when it is not).
+      commandline -rb '' # Force clear.
+      commandline -rb $command
+    end
+  end
+
+  bind -M default / reverse_history_search
 end
