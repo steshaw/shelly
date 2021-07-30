8c75699be8e082d5574caf7c59ccce639d600d87
Steven Shaw <steven@steshaw.org>
2021-07-30 13:55:15 +1000

    Fish: Tweak WakaTime project/entity

--- dotfiles/.config/fish/wakatime.fish
+++ dotfiles/.config/fish/wakatime.fish
@@ -1,24 +1,29 @@
 # From https://raw.githubusercontent.com/Cyber-Duck/fish-wakatime/master/fish-wakatime.sh
 
-# wakatime for fish
-#
-# Add this to the fish_prompt function in
-# ~/.config/fish/functions/fish_prompt.fish
-# (or if it doesn't exist, create it).
-
-
-# We've also included an example of how
-# to determine the current project from the pwd.
-# It'll only work without alterations if
-# you happen to keep all your projects in
-# ~/Sites/ on a Mac, but it's easy to modify
-
 set -l project
 
-if echo (pwd) | grep -qEi "^/Users/$USER/Sites/"
-    set  project (echo (pwd) | sed "s#^/Users/$USER/Sites/\\([^/]*\\).*#\\1#")
+if echo (pwd) | grep -qEi "$HOME/Code/.*/"
+    set project (echo (pwd) | sed "s-$HOME/Code/.*/\\([^/]*\\).*-\\1-")
 else
-    set  project "Terminal"
+    set project "Terminal"
 end
 
-wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 > /dev/null&
+set -l entity
+if true
+    set entity "$TERM.fish"
+else
+    set entity (echo $history[1] | cut -d ' ' -f1)
+end
+
+echo $project:$entity >>/tmp/wakatime.fish.log
+
+#wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 >> /tmp/wakatime.fish.log &
+wakatime \
+   --write \
+   --plugin "fish-wakatime/0.0.2" \
+   --project "$project" \
+   --entity-type 'app' \
+   --entity 'Fish' \
+   --language 'fish' \
+   --verbose \
+   2>&1 >> /tmp/wakatime.fish.log &
