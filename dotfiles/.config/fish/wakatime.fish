3d76c181f9115c5fadff4d4d7f380d9f1ef00b0f
Steven Shaw <steven@steshaw.org>
2021-07-30 17:43:25 +1000

    Fish: Remove commented out config

--- dotfiles/.config/fish/wakatime.fish
+++ dotfiles/.config/fish/wakatime.fish
@@ -18,7 +18,6 @@ end
 
 echo $project:$entity >>/tmp/wakatime.fish.log
 
-#wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 >> /tmp/wakatime.fish.log &
 wakatime \
    --write \
    --plugin "fish-wakatime/0.0.2" \
