61f40ed1200cfc44640e1cf8fe46d9fc427c7a32
Steven Shaw <steven@steshaw.org>
2021-08-24 22:34:04 +1000

    AutoKey: Narrow filters for compatibility with JetBrains

--- dotfiles/.config/autokey/data/My Phrases/Global/macOS Move Tab Left.py
+++
@@ -1,4 +0,0 @@
-if window.get_active_class() == 'jetbrains-goland.jetbrains-goland':
-  keyboard.send_keys("<ctrl>+<alt>+<right>")
-else:
-  keyboard.send_keys("<ctrl>+<shift>+<page_up>")
--- dotfiles/.config/autokey/data/My Phrases/Global/macOS Prev Tab.py
+++
@@ -1,4 +0,0 @@
-if window.get_active_class() == 'jetbrains-goland.jetbrains-goland':
-  keyboard.send_keys("<ctrl>+<shift>+]")
-else:
-  keyboard.send_keys("<ctrl>+<page_up>")
