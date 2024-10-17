70f5c818a6e271af6b082fcf9c1caed29d2df087
Steven Shaw <steven@steshaw.org>
2024-10-18 08:19:49 +1000

    Configure WezTerm

---
+++ files/home/.config/wezterm/wezterm.lua
@@ -0,0 +1,26 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Dracula'
+--config.color_scheme = 'AdventureTime'
+
+config.window_decorations = "NONE"
+
+config.font_size = 15.0
+
+-- -> => ++ <> >= <=
+config.font = wezterm.font {
+  family = 'Iosevka Term JBML',
+--  family = 'Jetbrains Mono Nerd Font',
+--  stretch = 'Expanded',
+--  weight = 'Regular',
+}
+
+-- and finally, return the configuration to wezterm
+return config
